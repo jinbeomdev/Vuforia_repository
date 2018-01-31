5742e4f54c94dc9e24b4d1895b1a5cae8588176b
jinbeomdev <niutn@naver.com>
2018-01-31 16:36:48 +0900

    init commit

---
+++ app/src/main/java/com/example/alchera/myapplication/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.alchera.myapplication;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.vuforia.CameraDevice;
+import com.vuforia.DataSet;
+import com.vuforia.Device;
+import com.vuforia.INIT_FLAGS;
+import com.vuforia.ObjectTarget;
+import com.vuforia.ObjectTracker;
+import com.vuforia.Renderer;
+import com.vuforia.STORAGE_TYPE;
+import com.vuforia.Trackable;
+import com.vuforia.Tracker;
+import com.vuforia.TrackerManager;
+import com.vuforia.Vec2I;
+import com.vuforia.VideoBackgroundConfig;
+import com.vuforia.Vuforia;
+
+import java.util.ArrayList;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends Activity {
+    ArrayList<String> mDataSetStrings = new ArrayList<String>();
+    DataSet mDataset;
+    GLSurfaceView mGLView;
+    GLSurfaceView.Renderer mRenderer;
+    ObjectTracker mObjectTracker;
+    Activity mActivity;
+    String licenceKey = "ATsUlQH/////AAAAmeFH2PdkqEWai6m8/mPhv5Qj9nWfD5KVKltkCzFeFfqJPNmUorPKkmqsr2Pk3h/DSPskgMG7CQauDqwMYZQOuXqZ/KPw50YzL0bnV1SkCEvPXiu33GIRJxFO71xdjdRcVlLuTaAeEyhd+45U08XfSKesbqbk2EnZpLyzo8vzIE+rldrwNbJh1yeDeyrMfFd0wzOFXvuwsEMqvZgu/mhJcR9+iKWkzwOVC2ePhc4xnY7pL+F4SUAm6BLDQr5OeQmujOaBtrzqRn9J/tGzCBMt/72uHh4aqoZFOp1YJpOFptHA/LrqnQt4uwMELv+hoO7eqZhk005Z8t8FxeL89T9ChTLPwvYP9KT5a92nSASKUHGF";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //OpenGL View
+        mGLView = new GLSurfaceView(this);
+        mGLView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        mGLView.setEGLContextClientVersion(2);
+
+        mRenderer = new GLSurfaceView.Renderer() {
+            Renderer mVuforiaRenderer = Renderer.getInstance();
+
+            @Override
+            public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl10) {
+
+            }
+        };
+
+        mGLView.setRenderer(mRenderer);
+        addContentView(mGLView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+        InitVuforiaTask initVuforiaTask = new InitVuforiaTask();
+        initVuforiaTask.execute();
+    }
+
+    class InitVuforiaTask extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            //Vuforia init
+            int mProgressValue = -1;
+
+            int initFlag = INIT_FLAGS.GL_20;
+            Vuforia.setInitParameters(MainActivity.this, initFlag, licenceKey);
+            do {
+                mProgressValue = Vuforia.init();
+            }while(!isCancelled() && mProgressValue >= 0 && mProgressValue < 100);
+
+            if(mProgressValue == -1) {
+                Log.e("hate", "init fail");
+            }
+
+            if(mProgressValue == 100) {
+                Log.e("init succ", "init succ");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            InitTrackerTask initTrackerTask = new InitTrackerTask();
+            initTrackerTask.execute();
+        }
+    }
+
+    class InitTrackerTask extends  AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            //Tracker init
+            TrackerManager tManager = TrackerManager.getInstance();
+            Tracker tracker = tManager.initTracker(ObjectTracker.getClassType());
+            if(tracker == null) {
+                Log.e("init Tracker", "fail");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            LoadTrackerData loadTrackerData = new LoadTrackerData();
+            loadTrackerData.execute();
+        }
+    }
+
+    class LoadTrackerData extends  AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            //Data load
+            mDataSetStrings.add("MyApplication.xml");
+            TrackerManager tManager = TrackerManager.getInstance();
+            mObjectTracker = (ObjectTracker) tManager.getTracker(ObjectTracker.getClassType());
+            mDataset = mObjectTracker.createDataSet();
+            mDataset.load(mDataSetStrings.get(0), STORAGE_TYPE.STORAGE_APPRESOURCE);
+            mObjectTracker.activateDataSet(mDataset);
+            Trackable trackable = mDataset.getTrackable(0);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            StartCameraAndTracker startCameraAndTracker = new StartCameraAndTracker();
+            startCameraAndTracker.execute();
+        }
+    }
+
+    class StartCameraAndTracker extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            //Camera init
+            VideoBackgroundConfig config = new VideoBackgroundConfig();
+            config.setEnabled(true);
+            config.setPosition(new Vec2I(0, 0));
+
+            if(!CameraDevice.getInstance().init(CameraDevice.CAMERA_DIRECTION.CAMERA_DIRECTION_DEFAULT)) {
+                Log.e("camera", "error1");
+            }
+
+            if(!CameraDevice.getInstance().selectVideoMode(CameraDevice.MODE.MODE_DEFAULT)) {
+                Log.e("camera", "error2");
+            }
+
+            if(!CameraDevice.getInstance().start()) {
+                Log.e("camera", "error3");
+
+            }
+
+            Renderer.getInstance().setVideoBackgroundConfig(config);
+
+            Log.e("camera", "camera succ");
+            //Tracker start
+            mObjectTracker.start();
+
+            return null;
+        }
+    }
+}
